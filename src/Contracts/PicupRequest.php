651e412307e7a8aacd9d4cd0c9cc05c889aa5088
Bryan Paddock <bryanpaddock@gmail.com>
2019-10-04 12:15:57 +0200

    Added classdoc to PicupRequest interface

--- src/Contracts/PicupRequest.php
+++ src/Contracts/PicupRequest.php
@@ -2,6 +2,11 @@
 
 namespace PicupTechnologies\PicupPHPApi\Contracts;
 
+/**
+ * Used by the PicupRequestException to group all PicupRequests together
+ *
+ * @package PicupTechnologies\PicupPHPApi\Contracts
+ */
 interface PicupRequest
 {
 
